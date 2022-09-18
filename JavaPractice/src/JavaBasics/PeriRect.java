42aff8ce1611fa74a0ac91be813859f719c104f1
Harshita <mailmeharshita24@gmail.com>
2022-09-18 23:48:06 +0530

    perimeter

---
+++ JavaPractice/src/JavaBasics/PeriRect.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+import java.util.*;
+
+public class PeriRect {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length");
+		int n = sc.nextInt();
+		System.out.println("Enter the breadth");
+		int m = sc.nextInt();
+		
+		int res = 2*(n+m);
+		System.out.println("The perimeter of the rectangle is "+ res);
+
+	}
+
+}
