927c151937e838f474081bf7b26ed3dd8f69a391
Harshita <mailmeharshita24@gmail.com>
2022-09-18 16:24:05 +0530

    second commit

--- JavaPractice/src/JavaBasics/PrimeNumber.java
+++ JavaPractice/src/JavaBasics/PrimeNumber.java
@@ -5,18 +5,21 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		//show if n is a prime  number or not?
 		int n = 13;
-		boolean isPrime = false;
+		boolean isPrime = true;
 		
-		for (int i = 3; i<n; i+=2) {
-			if(n % i != 0) {
-				isPrime = true;
+		for(int i = 2; i<n; i++) {
+			if(n%i == 0) {
+				isPrime= false;
 				break;
 			}
+		}
+		
+		System.out.println(isPrime);
 			
 		}
 			
 		
 
-	}
+	
 
 }
