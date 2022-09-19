8ab304258b71fb6e886045f833be8e506e82a274
Harshita <mailmeharshita24@gmail.com>
2022-09-19 20:42:41 +0530

    new commit

---
+++ JavaPractice/src/JavaBasics/FloatPoint.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+import java.util.*;
+
+public class FloatPoint {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a float number.");
+		float m  = sc.nextFloat();
+		System.out.println("Enter other float number.");
+		float n = sc.nextFloat();
+		
+		float p = m*n;
+		System.out.println("The number is "+ p);
+		sc.close();
+	}
+
+}
