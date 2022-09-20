a894d2d0855278045451412392bb33d1cc87e80e
Harshita <mailmeharshita24@gmail.com>
2022-09-21 00:33:54 +0530

    Solid rectangle pattern

---
+++ JavaPractice/src/JavaBasics/Star4.java
@@ -0,0 +1,22 @@
+// Solid rectangle star pattern. 
+package JavaBasics;
+import java.util.*;
+
+
+public class Star4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number.");
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("*");
+			sc.close();
+		}
+
+	}
+
+}
