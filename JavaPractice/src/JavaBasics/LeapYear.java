5a0b9177e0529fcdbe791a07273a5f85b9a964f4
Harshita <mailmeharshita24@gmail.com>
2022-09-20 14:27:13 +0530

    leapyear prog

---
+++ JavaPractice/src/JavaBasics/LeapYear.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+import java.util.*;
+
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter any year to check the leap year.");
+		int n = sc.nextInt();
+		boolean isLeapYear = false;
+		
+		if((n%4 == 0) || (n%400 ==  0)) {
+			isLeapYear = true;
+		}
+		
+		System.out.println(isLeapYear);
+		sc.close();
+	}
+}
