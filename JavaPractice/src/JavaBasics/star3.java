8a5630b61be0c7e8b4a38c42694817373f0369ea
Harshita <mailmeharshita24@gmail.com>
2022-09-20 19:57:02 +0530

    star pattern

---
+++ JavaPractice/src/JavaBasics/star3.java
@@ -0,0 +1,24 @@
+package JavaBasics;
+import java.util.Scanner;
+
+public class star3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k =1;k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			sc.close();
+		}
+
+	}
+
+}
