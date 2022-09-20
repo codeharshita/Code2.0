0720bc1ad48455032faf9e238e0cb350740328e3
Harshita <mailmeharshita24@gmail.com>
2022-09-20 11:04:53 +0530

    new commit

---
+++ JavaPractice/src/JavaBasics/SumNatural.java
@@ -0,0 +1,23 @@
+package JavaBasics;
+import java.util.*;
+
+public class SumNatural {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number:-");
+		int n = sc.nextInt();
+		
+		int i = 1;
+		int sum = 0;
+		
+		while(i<=n) {
+			sum = sum+i;
+			i++;
+			}
+		System.out.println("Th sum of first " + n + " numbers is "+ sum);
+		sc.close();
+	}
+
+}
