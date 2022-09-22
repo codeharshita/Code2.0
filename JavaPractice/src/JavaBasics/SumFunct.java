969ec2203eb3760c6bc6594cc8c51f283543d465
Harshita <mailmeharshita24@gmail.com>
2022-09-22 17:31:35 +0530

    new commit

--- JavaPractice/src/JavaBasics/SumFunct.java
+++ JavaPractice/src/JavaBasics/SumFunct.java
@@ -3,19 +3,19 @@ import java.util.*;
 
 public class SumFunct {
 	
-	public static int printSum(int a, int b) {
-		int c = a+b;
-		return c;
+	public static int printSum(int number1, int number2) {
+		int numberResult = number1+number2;
+		return numberResult;
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the first number.");
-		int a = sc.nextInt();
+		int number1 = sc.nextInt();
 		System.out.println("Enter the second number.");
-		int b = sc.nextInt();
+		int number2 = sc.nextInt();
 		
-		System.out.println("The sum is: "+ printSum(a,b));
+		System.out.println("The sum is: "+ printSum(number1,number2));
 		sc.close();
 		
 
