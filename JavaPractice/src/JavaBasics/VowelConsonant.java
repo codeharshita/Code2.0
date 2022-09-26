91a513e7de9640a8b06a99e2c90a5c7b5ced45db
Harshita <mailmeharshita24@gmail.com>
2022-09-26 14:53:25 +0530

    VowelConsonant

--- JavaPractice/src/JavaBasics/VowelConsonant.java
+++ JavaPractice/src/JavaBasics/VowelConsonant.java
@@ -1,11 +1,25 @@
 package JavaBasics;
 import java.util.Scanner;
 
+/**
+ * VowelConsonant is a Java Program to
+ *  Check Whether the Character is Vowel or Consonant
+ * */
+
 public class VowelConsonant { 
-	public static char printVowelConsonant(char c) {
-		
 	
-		if(c == 'a' || c =='e' || c == 'i' || c == 'o' || c == 'u' ) {
+	/**
+	 * This methodis used to print or differentiate 
+	 * between consonants and vowels of a character.
+	 * @param character This is an argument which 
+	 * 					is provided by the user.
+	 * 
+	 * @return char This returns the character defined ny the user.
+	 * */
+	public static char printVowelConsonant(char character) {
+		
+	  // OR oerator is used.
+		if(character == 'a' || character =='e' || character == 'i' || character == 'o' || character == 'u' ) {
 		System.out.println("It's a vowel");
 		}else
 		{
@@ -13,14 +27,16 @@ public class VowelConsonant {
 		}
 		
 		
-		return c;
+		return character;
 	}
 
 	public static void main(String[] args) {
+		// Scanner user input.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a character");
-		char c = sc.next().charAt(0);
-		printVowelConsonant( c);
+		char character = sc.next().charAt(0);
+		//Method calling.
+		printVowelConsonant( character);
 		sc.close();
 		
 	}
