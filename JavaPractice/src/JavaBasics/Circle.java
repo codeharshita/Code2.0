a4dc8e651b65ab293a640085964e32d3e119cde6
Harshita <mailmeharshita24@gmail.com>
2022-09-27 12:20:47 +0530

    CircleCircumference

---
+++ JavaPractice/src/JavaBasics/Circle.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Circle {
+	
+	public static double printCircumference(int radius) {
+		return 2*Math.PI*radius;
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the radius.");
+		int radius = sc.nextInt();
+		System.out.println("The circumference of circle is "+ printCircumference(radius));
+		sc.close();
+
+	}
+
+}
