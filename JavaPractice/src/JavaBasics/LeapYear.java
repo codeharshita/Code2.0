b76bba788b19687948883e914dca6c6ddf65d61c
Harshita <mailmeharshita24@gmail.com>
2022-09-28 15:29:27 +0530

    new commits

--- JavaPractice/src/JavaBasics/LeapYear.java
+++ JavaPractice/src/JavaBasics/LeapYear.java
@@ -2,6 +2,7 @@ package JavaBasics;
 import java.util.*;
 
 
+
 public class LeapYear {
 
 	public static void main(String[] args) {
---
+++ JavaPractice/src/JavaBasics/Switch.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		
+		switch(2) {
+		case 9:
+			System.out.println("This is valid");
+			break;
+		case 2: 
+			System.out.println("This is not valid");
+			break;
+		default: 
+			System.out.println("Unknown number");
+		}
+
+	}
+
+}
