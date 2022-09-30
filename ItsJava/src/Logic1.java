dcc1cb1da7be627c4e3b430821a0a215d112a039
Harshita <mailmeharshita24@gmail.com>
2022-09-30 19:36:11 +0530

    new commit

---
+++ ItsJava/src/Logic1.java
@@ -0,0 +1,19 @@
+
+public class Logic1 {
+
+	public static void main(String[] args) {
+		int num = 111;
+		int cubeSum = 0;
+		while(num != 0) {
+			int r = num % 10;
+			num= num /10;
+			//System.out.println(r);
+			cubeSum = (int) ((int) cubeSum + Math.pow(r, 3));
+			System.out.println(cubeSum);
+		}
+
+
+
+	}
+
+}
