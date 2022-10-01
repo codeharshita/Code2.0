8216b02489183e1c9e4fc40d728495563e38cf21
Harshita <mailmeharshita24@gmail.com>
2022-10-01 08:32:39 +0530

    updated cubeSum

--- JavaPractice/src/JavaBasics/CubeSum.java
+++ JavaPractice/src/JavaBasics/CubeSum.java
@@ -1,29 +1,27 @@
 // Write a java program to print the sum of cube of digits.
 
 package JavaBasics;
+import java.util.*;
 // CubeSum is a classname.
 
 public class CubeSum {
 	
-	// 
-
-	public static void main(String[] args) {
-		 int num = 51;
-	        int sum = 0;
-	        int cubeSum = 0;
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number.");
+        int num = sc.nextInt();
+	        
+		 int cubeSum = 0;
 
 	        while( num!= 0){
 	            int remainder = num % 10;
-	            sum = sum + remainder;
-	            cubeSum = (int) (cubeSum + Math.pow(sum,3));
 	            num = num / 10;
-	        }
+	            cubeSum = (int) (cubeSum + Math.pow(remainder,3));
+	           
+	         }
 	        System.out.println(cubeSum);
-	        
-
-
-	}
+	        sc.close();
+     }
 
 }
 
-// Doubt-  Answer should be 126
