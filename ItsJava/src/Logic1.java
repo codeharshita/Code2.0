583dac90c28968d13e6b78dbef67825b34cdf79e
Harshita <mailmeharshita24@gmail.com>
2022-10-01 08:27:20 +0530

    updated

--- ItsJava/src/Logic1.java
+++ ItsJava/src/Logic1.java
@@ -4,16 +4,15 @@ public class Logic1 {
 	public static void main(String[] args) {
 		int num = 51;
 		int cubeSum = 0;
+		
+	
 		while(num != 0) {
 			int r = num % 10;
 			num= num /10;
-			//System.out.println(r);
 			cubeSum = (int) ((int) cubeSum + Math.pow(r, 3));
 		
 		}
-		System.out.println(cubeSum);
-
-
-	}
+				System.out.println(cubeSum);
+		}
 
 }
