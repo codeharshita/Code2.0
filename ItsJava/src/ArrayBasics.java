09ee42db991f3ffaef7c94875bcfe90edf2a1c98
Harshita <mailmeharshita24@gmail.com>
2022-10-02 09:34:49 +0530

    Arrar Basic

---
+++ ItsJava/src/ArrayBasics.java
@@ -0,0 +1,28 @@
+
+public class ArrayBasics {
+
+	public static void main(String[] args) {
+	 
+	  
+	  // declare and allocate the memory using a single statement.
+		int[] arr = new int[5];
+	 
+	// initilaizing the elements of array.
+	 arr[0]= 1;
+	 arr[1]= 3;
+	 arr[2]= 4;
+	 arr[3]= 9;
+	 arr[4]= 8;
+	 
+	 // accessing the elements of array.
+	 System.out.println("Accessing the elements of the array.");
+	 System.out.println();
+	 System.out.println("First element in array "+ arr[0]);
+	 System.out.println("Second element in array "+ arr[1]);
+	 System.out.println("Third element in array " + arr[2]);
+	 System.out.println("Fourth element in array "+ arr[3]);
+	 System.out.println("Fifth element in array "+ arr[4]);
+
+	}
+
+}
