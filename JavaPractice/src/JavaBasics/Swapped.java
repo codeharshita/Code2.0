3d67092a022a978c5feb0c585ff363e65157490e
Harshita <mailmeharshita24@gmail.com>
2022-10-04 22:26:51 +0530

    Swapped Numbers

---
+++ JavaPractice/src/JavaBasics/Swapped.java
@@ -0,0 +1,21 @@
+// Java program for swapping numbers.
+package JavaBasics;
+
+
+public class Swapped {
+
+	public static void main(String[] args) {
+				int a = 2;
+				int b = 5;
+				
+				int temp ;
+			System.out.println("The number before swapping is "+ a + b);
+				temp = a;
+				a = b;
+				b = temp;
+				
+			System.out.println("The number after swapping is "+ a + b );
+			
+	}
+
+}
