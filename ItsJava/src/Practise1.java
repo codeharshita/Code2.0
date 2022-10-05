480a9c922718ed3d7998a4f143b1ac81786c5f2f
Harshita <mailmeharshita24@gmail.com>
2022-10-05 13:30:31 +0530

    Syntax checking arrays

---
+++ ItsJava/src/Practise1.java
@@ -0,0 +1,18 @@
+// Syntax checking for arrays.
+public class Practise1 {
+	
+
+	public static void main(String[] args) {
+		int[] arr = new int[] {1,4,2,3};
+		
+		for(int a: arr ) {
+			System.out.print(" "+a);
+		}
+		System.out.println();
+		
+		for(int i = 0; i<arr.length; i++) {
+			System.out.print( " "+ arr[i] );
+	 }		
+
+	}
+}
