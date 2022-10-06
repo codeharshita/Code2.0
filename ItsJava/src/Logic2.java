f79b9cafc64f5a39155452c96256928a34b15e34
Harshita <mailmeharshita24@gmail.com>
2022-10-06 15:05:43 +0530

    JavaLogics

---
+++ ItsJava/src/Logic2.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Logic2 {
+
+	public static void main(String[] args) {
+		int[] even = {2,4,6,8};
+		int[] odd= {1,3,5,7};
+		
+		
+		
+		
+
+	}
+
+	
+}
--- ItsJava/src/Practise1.java
+++ ItsJava/src/Practise1.java
@@ -1,4 +1,5 @@
 // Syntax checking for arrays.
+import java.util.*;
 public class Practise1 {
 	
 
@@ -11,8 +12,13 @@ public class Practise1 {
 		System.out.println();
 		
 		for(int i = 0; i<arr.length; i++) {
-			System.out.print( " "+ arr[i] );
-	 }		
+			System.out.print( " "+ arr[i]);
+	 }
+		System.out.println();
+		
+		//for(int i = 0; i<arr.length; i++) {
+			System.out.println(Arrays.toString(arr));
+		//}
 
 	}
 }
