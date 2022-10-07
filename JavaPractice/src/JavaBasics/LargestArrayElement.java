e210220f883cc747a8cbd1c49ffe788b6703acee
Harshita <mailmeharshita24@gmail.com>
2022-10-07 20:12:08 +0530

    LargestArrayElement

--- JavaPractice/src/JavaBasics/LargestArrayElement.java
+++ JavaPractice/src/JavaBasics/LargestArrayElement.java
@@ -1,22 +1,23 @@
 package JavaBasics;
 
 public class LargestArrayElement {
+	public static int getLargest(int [] number) {
 	
+		int max = 0;
+		for(int i =0; i< number.length; i++) {
+			if(number[i]>max) {
+				max=number[i];
+			}
+		}
+		return max;
+	}
 	
 	
 
 	public static void main(String[] args) {
-		
-		 int arr[] = {10,324,45, 7886,1234};
-	        int max = arr[0];
-
-	        for(int i=0; i<arr.length; i++){
-	            if(arr[i]> max){
-	                 max = arr[i];
-	                System.out.println(max);
-	            }
-	        }
-
+		int[] arr = new int[] {5,9,23,401};
+			int res = getLargest(arr);
+		System.out.println(res+ " is the largest array element.");
 	}
 
 }
