0ae60eef7b7dc6030b2adc5320dcd0cda7de90c9
Harshita <mailmeharshita24@gmail.com>
2022-10-08 09:54:22 +0530

    practice

--- ItsJava/src/Logic3.java
+++ ItsJava/src/Logic3.java
@@ -4,6 +4,8 @@ public class Logic3 {
 	public static void main(String[] args) {
 		int B[]= {2,1,4,3,5};
 		
+		System.out.println(B);
+		
 		
 		for(int i = 0; i<B.length; i++) {
 			System.out.println(B[i]++);
