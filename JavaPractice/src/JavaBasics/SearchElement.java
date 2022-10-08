44370214b5947b6fc6c3e08dbce4dc27aace7de0
Harshita <mailmeharshita24@gmail.com>
2022-10-08 11:32:03 +0530

    SearchArrayElemnt

--- JavaPractice/src/JavaBasics/SearchElement.java
+++ JavaPractice/src/JavaBasics/SearchElement.java
@@ -1,25 +1,45 @@
 package JavaBasics;
+import java.util.Scanner;
 
 public class SearchElement {
 
 	
+	public static void searchElement(int [] arr, int key) {
+		
+		boolean flag = false;
+		
+		for(int i = 0; i<arr.length; i++) {
+			if(key == arr[i]) {
+				System.out.println("Element found");
+				flag= true;
+				break;
+			}
+		}
+		if(flag == false) {
+			System.out.println("Element not found.");
+		}
+		
+	}
 	
 	public static void main(String[] args) {
-		
-	int[] arr = {4,6,1,3,5};
-	int key = 10;
-	boolean flag  = false;
-	
-	for(int i = 0;i<arr.length; i++) {
-		if(key == arr[i]) {
-			System.out.println("Element found");
-				flag =true;
-				break;
-		}
-	}
-	if(flag == false)
-	System.out.println("Element not found");
+		Scanner sc = new Scanner(System.in);
 
+		int[] arr = new int[5];
+		
+		for(int i= 0 ; i<arr.length; i++) {
+			arr[i]= sc.nextInt();
+		}
+		System.out.println("Array Entered.");
+		for(int x: arr) {
+			System.out.println(x);
+		}
+		System.out.println("Write an element to be searched.");
+		int key = sc.nextInt();
+		
+		searchElement(arr, key);
+		sc.close();
+		
+		
 	}
 
 }
