6cc6bfcfd09820a4cb7fa1cf13a84f4557f50bd4
Harshita <mailmeharshita24@gmail.com>
2022-10-08 12:00:46 +0530

    MaxElementSearch

---
+++ JavaPractice/src/JavaBasics/MaxElement.java
@@ -0,0 +1,26 @@
+package JavaBasics;
+import java.util.*;
+
+public class MaxElement {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int maxElement=0;
+		int [] arr = new int[10];
+		System.out.println("Enter any 10 array elements. ");
+		
+		for(int i = 0; i<arr.length; i++) {
+			arr[i]= sc.nextInt();
+		}
+
+		Arrays.sort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+		maxElement = arr[10-1];
+		System.out.println("The maximum element is "+ maxElement);
+		sc.close();
+		
+		
+	}
+
+}
