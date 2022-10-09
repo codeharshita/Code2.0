081dc8b746160588288076234c04fcb248b23082
Harshita <mailmeharshita24@gmail.com>
2022-10-09 22:20:25 +0530

    DescArrayElements

--- ItsJava/src/DescOrder.java
+++ ItsJava/src/DescOrder.java
@@ -1,7 +1,10 @@
 // Print array elements in descending order.
+
+
 import java.util.*;
 public class DescOrder {
 
+	
 	public static void main(String[] args) {
 		Integer [] arr  = {23,76,12,7,99,74};
 		Arrays.sort(arr, Collections.reverseOrder());
