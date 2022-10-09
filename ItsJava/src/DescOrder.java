a371767c74f1976259ed1a8068e5b9b88840b506
Harshita <mailmeharshita24@gmail.com>
2022-10-09 09:14:08 +0530

    DescendingOrder

---
+++ ItsJava/src/DescOrder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DescOrder {
+
+	public static void main(String[] args) {
+		Integer [] arr  = {23,76,12,7,99,74};
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		
+		System.out.println("Descending order.");
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
