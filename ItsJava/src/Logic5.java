cda317a7579af549a3f25c16b7c06457c5b0ffb4
Harshita <mailmeharshita24@gmail.com>
2022-10-11 22:58:52 +0530

    RemoveDuplicateElements

---
+++ ItsJava/src/Logic5.java
@@ -0,0 +1,21 @@
+// Logic is to remove the duplicate array elements.
+import java.util.*;
+public class Logic5 {
+
+	public static void main(String[] args) {
+		int[] arr = {2,1,2,2,2,4,5};
+		int remove = 2;
+		//Sorting	
+		Arrays.sort(arr);
+			System.out.println(Arrays.toString(arr));
+
+			for(int i =0 ; i<arr.length; i++) {
+				if(remove == arr[i]) {
+					continue;
+				}
+				System.out.println(arr[i]);
+			}
+
+	}
+
+}
