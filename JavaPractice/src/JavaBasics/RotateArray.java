14f6a630650c2ef2576224f96d6a436ff9540e88
Harshita <mailmeharshita24@gmail.com>
2022-10-13 23:30:25 +0530

    Updated shifting array elements

--- JavaPractice/src/JavaBasics/RotateArray.java
+++ JavaPractice/src/JavaBasics/RotateArray.java
@@ -1,12 +1,19 @@
 package JavaBasics;
-
+// Remove the arr[3] value.
 public class RotateArray {
 
 	public static void main(String[] args) {
 		int [] arr= {5,9,6,10,12,7,3,5,4,2};
 		int temp  = arr[0];
+		int remove = 10;
+	  
+	
 		
-		for(int i = 1; i<arr.length;i++) {
+		for(int i = 0; i<arr.length;i++) {
+			if(arr[i] == remove) {
+				continue;
+			}
+	
 			System.out.print(arr[i]+ " ");
 		}
 				arr[arr.length-1]= temp;
