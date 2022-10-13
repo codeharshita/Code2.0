26e73274cce37024402b49433e778d1e04e44083
Harshita <mailmeharshita24@gmail.com>
2022-10-13 23:07:57 +0530

    Shifting Logic

---
+++ ItsJava/src/Logic6Shifting.java
@@ -0,0 +1,19 @@
+// Shifting the array elements by 1.
+public class Logic6Shifting {
+
+	public static void main(String[] args) {
+	   int[] a= {4,7,8,9,6,3};
+	   int temp = a[0];
+	   
+	   for(int i =1; i<a.length; i++) {
+		   System.out.print(a[i]+ " ");
+	   }
+	   			
+	   a[a.length-1] = temp;
+	   System.out.println(temp);
+	   
+	   
+
+	}
+
+}
