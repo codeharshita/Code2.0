7acbe26daa1a1bbf4db4a2dfc3b1a72e48257cc8
Harshita <mailmeharshita24@gmail.com>
2022-10-15 14:57:30 +0530

    Basic UserInput TwoDArray implementation

---
+++ JavaPractice/src/JavaBasics/TwoDArray2.java
@@ -0,0 +1,33 @@
+package JavaBasics;
+import java.util.Scanner;
+
+public class TwoDArray2 {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the array elements.");
+			int rows = sc.nextInt();
+			int cols = sc.nextInt();
+			int[][] arr = new int [rows][cols];
+			
+			
+			//input
+			//rows
+			for(int i =0; i<rows;i++) {
+				//columns
+				for(int j = 0; j<cols; j++) {
+					arr[i][j]= sc.nextInt();
+				}
+			}
+			// output
+			
+			for(int i = 0; i<rows;i++) {
+				for(int j =0; j<cols; j++){
+					System.out.print(arr[i][j]+" ");
+				}
+				System.out.println();
+			}
+
+	}
+
+}
