1a80c51decbce6792960ffd63b102e670845eaa1
Harshita <mailmeharshita24@gmail.com>
2022-10-21 23:54:17 +0530

    Created a 2-D matrix

---
+++ JavaPractice/src/JavaBasics/Practise1.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+public class Practise1 {
+
+	public static void main(String[] args) {
+	
+		int[][] arr = {
+				{1,2,3},{4,5,6}
+		        };
+		
+		for(int i=0; i<arr.length;i++) {
+				for(int j = 0; j<arr[0].length; j++) {
+				   System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+		}
+
+	}
+
+}
