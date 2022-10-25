a1931f3fb38ffc545cc66162f59924cfe0daf2eb
Harshita <mailmeharshita24@gmail.com>
2022-10-25 08:03:33 +0530

    Maxmimum Element in Array.

---
+++ JavaPractice/src/JavaBasics/MaxElement2.java
@@ -0,0 +1,26 @@
+package JavaBasics;
+
+public class MaxElement2 {
+	
+	
+	public static int maxElement(int [] Arr) {
+		int max = Arr[0];
+		
+		for(int i =0;i<Arr.length;i++) {
+			if(Arr[i]>max) {
+				max= Arr[i];
+//				System.exit(0);
+			}
+		}
+		System.out.println("Just Found");
+		return max;
+	}
+
+	public static void main(String[] args) {
+		
+		int[] Arr  = {3,9,7,8,12,6,15,5,4,10};
+		System.out.println(maxElement(Arr)+ " is the largest element.");
+
+	}
+
+}
