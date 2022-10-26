f32817b81bffea540986a1145341daeeac9c4279
Harshita <mailmeharshita24@gmail.com>
2022-10-26 23:07:09 +0530

    Class documentation

--- Oops/src/Bicycle.java
+++ Oops/src/Bicycle.java
@@ -1,10 +1,18 @@
 //Understanding class.
+/**
+ * A class is a blueprint of the object which
+ * contains fields and methods.
+ * 
+ * fields are generally used to store data
+ * methods are generally used to show the functionality on that data.
+ * */
 public class Bicycle {
 	
 	//field
 	public static int wheels = 2;
 	
 	// method.
+	//We use static to save memory.
 	public static void stop() {
 		System.out.println("Stop!");
 	}
@@ -13,7 +21,7 @@ public class Bicycle {
 	public static void main(String[] args) {
 		System.out.println("The bicycle has "+ wheels + " wheels.");
 		stop();
-		//System.out.println(stop());
+		//System.out.println(stop());-> not applicable because it is 
 
 	}
 
