e7f0616cce73ebb97a278961ec0b299eee9df6ed
Harshita <mailmeharshita24@gmail.com>
2022-10-27 17:32:35 +0530

    Lamp methods, objects and class.

--- Oops/src/Lamp.java
+++ Oops/src/Lamp.java
@@ -1,13 +1,17 @@
 
 public class Lamp {
 	
+	//tells whether the light is on/off.
+	
 	boolean isOn ;
 	
+	//Method - TurnOn when the light is on 
 	void turnOn() {
 		isOn= true;
 		System.out.println("Is lamp on? "+ isOn);
 	}
 	
+	//Method - TurnOff when the light is off. 
 	void turnOff() {
 		isOn=false;
 		System.out.println("Is lamp off? " + isOn);
@@ -15,9 +19,12 @@ public class Lamp {
 
 	public static void main(String[] args) {
 
+		
+		//Creating objects to get the access to the methods led and halogen.
 		Lamp led = new Lamp();
 		Lamp halogen = new Lamp();
 		
+		// Calling Method using objects created NOT STATIC.
 		led.turnOn();
 		halogen.turnOff();
 
