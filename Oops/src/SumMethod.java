3e9b6c79d6d7757651d5fc8d63bfe5e01544fa6c
Harshita <mailmeharshita24@gmail.com>
2022-10-29 08:46:13 +0530

    Understanding local variables.

--- Oops/src/SumMethod.java
+++ Oops/src/SumMethod.java
@@ -2,7 +2,7 @@
 public class SumMethod {
 
 	/**
-	 * Non-static method.
+	 * Non-static method and it contains a local variable sum.
 	 * */
 	public int sumNum(int num1, int num2) { 
 		  return num1+num2;
