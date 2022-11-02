4b140242ec693641ec8de37d1721ac2411b7c9cd
Harshita <mailmeharshita24@gmail.com>
2022-11-02 21:11:32 +0530

    updated classes and objects

--- Oops/src/Animals.java
+++ Oops/src/Animals.java
@@ -6,7 +6,7 @@ public class Animals {
 	String color = "YellowishBrown";
 
 	public static void main(String[] args) {
-		Animals tiger = new Animals();
+		Animals tiger = new Animals(); 
 		System.out.println("This breed of tiger has "+ tiger.numberOfLegs + " legs.");
 		System.out.println("It has "+tiger.eyes+" eyes.");
 		System.out.println("The skin shade is "+ tiger.color+" color.");
