eb420b5c86aa429c18ae877197dd59ded0ca3040
Harshita <mailmeharshita24@gmail.com>
2022-11-02 22:25:27 +0530

    class attributes

--- Oops/src/ClassAttributes.java
+++ Oops/src/ClassAttributes.java
@@ -1,12 +1,15 @@
 
 public class ClassAttributes {
 	int x = 5;
-
+	int y = 7;
+	
+	
 	public static void main(String[] args) {
 		ClassAttributes at = new ClassAttributes();
 		int res = at.x;
-		System.out.println(res);
-				
+		int res2 = at.y;
+		System.out.println(res + " is the result");
+		System.out.println(res2+ " is the result.");
 
 	}
 
