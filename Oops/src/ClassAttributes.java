ec9c37c74492392c51016be7b6799db457892f23
Harshita <mailmeharshita24@gmail.com>
2022-11-02 22:22:51 +0530

    ClassAttributes

---
+++ Oops/src/ClassAttributes.java
@@ -0,0 +1,13 @@
+
+public class ClassAttributes {
+	int x = 5;
+
+	public static void main(String[] args) {
+		ClassAttributes at = new ClassAttributes();
+		int res = at.x;
+		System.out.println(res);
+				
+
+	}
+
+}
