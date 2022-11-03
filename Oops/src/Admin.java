5680501dc88e9bdb2dd8bbe30de83fdef997fc8c
Harshita <mailmeharshita24@gmail.com>
2022-11-04 00:09:32 +0530

    updated

--- Oops/src/Admin.java
+++ Oops/src/Admin.java
@@ -1,6 +1,7 @@
-
+// seperate class to access in the other class called main.java
 public class Admin {
 
+	//instance variable.
 	String name = "Supriya";
 	int age= 23;
 	String emailid ="Supriya234@gmail.com";
