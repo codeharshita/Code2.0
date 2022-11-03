e4393cd359f0a58c340e0e6693c6db29e0adbcd9
Harshita <mailmeharshita24@gmail.com>
2022-11-03 23:48:56 +0530

    Dog class and object

---
+++ Oops/src/Dog.java
@@ -0,0 +1,19 @@
+
+public class Dog {
+	
+	int eyes=2;
+	final int legs=4;
+	String color= "white";
+
+	public static void main(String[] args) {
+		Dog dg = new Dog();
+	//	int legs = 4;
+		System.out.println(dg.color);
+		System.out.println(dg.eyes);
+		System.out.println(dg.legs);
+		System.exit(0);
+		
+
+	}
+
+}
