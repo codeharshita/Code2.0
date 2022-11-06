79c0d62ee5157f681a6216d7f6f074cd6784f586
Harshita <mailmeharshita24@gmail.com>
2022-11-06 21:34:34 +0530

    oops

---
+++ Oops/src/Cmd.java
@@ -0,0 +1,10 @@
+
+public class Cmd {
+
+	public static void main(String[] args) {
+		System.out.println("Argument = " + args[0] );
+		System.out.println("Argument = "+ args[1]);
+
+	}
+
+}
---
+++ Oops/src/Constructor1.java
@@ -0,0 +1,24 @@
+
+public class Constructor1 {
+	
+	
+		
+		 int height;
+		 int weight;
+		 String color;
+	
+	
+
+	public static void main(String[] args) {
+		Constructor1 c = new Constructor1();
+		c.color = "Red";
+		c.height=5;
+		c.weight=76;
+		
+		System.out.println(c.color);
+		System.out.println(c.weight);
+		System.out.println(c.height);
+			
+	}
+
+}
