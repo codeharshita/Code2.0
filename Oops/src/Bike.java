f5cc4fd51135755f1b14fe3a18d2e5d1c3270c09
Harshita <mailmeharshita24@gmail.com>
2022-11-10 18:14:21 +0530

    New Constructor commit

---
+++ Oops/src/Bike.java
@@ -0,0 +1,23 @@
+// Write a constructor program.
+class Bike {
+	
+	String name;
+	int wheels;
+	String model;
+	String color;
+	boolean isActive;
+	
+	// To initialize the ready-made value we will nuit constructor.
+	
+	public Bike(String name, int wheels, String model, String color, boolean isActive) {
+		this.name= name;
+		this.wheels= wheels;
+		this.model= model;
+		this.isActive=isActive;
+		this.color= color;
+	}
+	
+	
+
+
+}
--- Oops/src/Main2.java
+++ Oops/src/Main2.java
@@ -8,7 +8,12 @@ public class Main2 {
 		pt.qty=3;
 		
 		pt.setPrice(100000);
-		
+		Bike n = new Bike("Honda", 4,"XZ45801","White",true);
+		System.out.println(n.color);
+		System.out.println(n.model);
+		System.out.println(n.name);
+		System.out.println(n.wheels);
+		System.out.println(n.isActive);
 
 		
 	}
