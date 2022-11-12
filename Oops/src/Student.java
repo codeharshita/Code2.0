49ceb4ac0c9979d1285f750341f4d7e13c11820b
Harshita <mailmeharshita24@gmail.com>
2022-11-12 14:17:07 +0530

    updated

--- Oops/src/Student.java
+++ Oops/src/Student.java
@@ -7,20 +7,21 @@ public class Student {
 	int sub1,sub2,sub3,sub4,sub5;
 	
 	
-	public void printTotal() {
+	public  void printTotal() {
 		System.out.println(" The total of subjects is printed.");
 	}
 
-	public void printAverage() {
+	public  void printAverage() {
 		System.out.println(" Print the average.");
 	}
 	
 	
 	public static void main(String[] args) {
-		Student ram = new Student();
+	Student ram = new Student();
 		ram.printAverage();
 		ram.printTotal();
 		System.exit(0);
+		
 
 	}
 
