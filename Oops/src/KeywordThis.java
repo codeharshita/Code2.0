07c2093e1e50e450b128eb6e6071a66a371b1ad4
Harshita <mailmeharshita24@gmail.com>
2022-11-14 20:45:03 +0530

    Understanding this keyword.

---
+++ Oops/src/KeywordThis.java
@@ -0,0 +1,17 @@
+//Understanding this keywrord.
+public class KeywordThis {
+	
+	int age;
+	
+	public KeywordThis(int age){
+		this.age= age;
+		
+	}	
+	public static void main(String args[]) {
+		KeywordThis karan = new KeywordThis(8);
+		System.out.println("Karan's age: " + karan.age);
+		
+		
+	}
+
+}
