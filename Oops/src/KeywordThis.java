60a727673099414567111f9e30a2066612fce53a
Harshita <mailmeharshita24@gmail.com>
2022-11-14 20:53:44 +0530

    this keyword concept

--- Oops/src/KeywordThis.java
+++ Oops/src/KeywordThis.java
@@ -1,4 +1,14 @@
 //Understanding this keyword.
+/*
+ * This is used to refer the current object of the class.
+ * It is similar to the object concept.
+ * Object is also called using dot operator ,similarly this is also called with the help
+ * of dot operator.
+ * 
+ * it can used while removing the ambiguity of the class variable.
+ * it can also be used while invoking the constructor or method.
+ *  
+ * */
 public class KeywordThis {
 	
 	int age;
