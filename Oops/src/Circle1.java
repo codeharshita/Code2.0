6fb3a2000776a9d9badb49b3009ff3fbbe9a0b65
Harshita <mailmeharshita24@gmail.com>
2022-11-15 23:30:24 +0530

    Inheritance

--- Oops/src/Circle1.java
+++ Oops/src/Circle1.java
@@ -27,7 +27,22 @@ class Cylinder2 extends Circle{
 }
 
 
-
+/*Inheritance:-
+ * Means acquiring the properties from the existing class and adding up
+ * with the new features in a new class .
+ * 
+ * It has SuperClass and SubClass.
+ * 		  BaseClass and DerivedClass.
+ * 		  ParentClass and ChildClass.
+ * 
+ * It is a Top Down approach.
+ * 
+ * Specialization is achieved using Inheritance.
+ * 
+ * Inheritance uses the extends keyword to acquire the properties from the existing class.
+ * 
+ * 
+ * */
 
 public class Circle1 {
 
