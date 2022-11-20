597f6ce2ae3e9c1340cb124260333579f971299b
Harshita <mailmeharshita24@gmail.com>
2022-11-20 07:34:30 +0530

    Abstract class example

---
+++ Oops/src/AbstractExample.java
@@ -0,0 +1,31 @@
+//abstract class.
+abstract class Super{
+	Super(){
+		System.out.println("Print Super.");
+	}
+	public void meth1() {
+		System.out.println("Meth1");
+	}
+	
+	abstract public void meth2();
+}
+//concrete class.
+class Sub extends Super{
+	
+	@Override
+	public void meth2() {
+		System.out.println("This is overrided meth2");
+	}
+	
+}
+
+public class AbstractExample {
+
+	public static void main(String[] args) {
+		Super s1 = new Sub();
+		s1.meth1();
+		s1.meth2();
+
+	}
+
+}
