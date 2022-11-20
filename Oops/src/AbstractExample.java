93b54e65929dfdc74da3ad6e4810ca2ef26e9df7
Harshita <mailmeharshita24@gmail.com>
2022-11-20 09:39:06 +0530

    Abstract Classes and Method Example.

--- Oops/src/AbstractExample.java
+++ Oops/src/AbstractExample.java
@@ -1,17 +1,24 @@
 //abstract class.
 abstract class Super{
 	Super(){
-		System.out.println("Print Super.");
+		System.out.println(" Super Cnstructor.");
 	}
 	public void meth1() {
 		System.out.println("Meth1");
 	}
 	
+	//abstract method.
 	abstract public void meth2();
 }
 //concrete class.
 class Sub extends Super{
 	
+	Sub(){
+		System.out.println("Sub constructor");
+	}
+	
+	
+	//Method overriding
 	@Override
 	public void meth2() {
 		System.out.println("This is overrided meth2");
@@ -25,6 +32,7 @@ public class AbstractExample {
 		Super s1 = new Sub();//Dynamic method dispatch.
 		s1.meth1();
 		s1.meth2();
+	
 
 	}
 
