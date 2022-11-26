169e59023235f40276b5cdef1cc5e468345d2475
Harshita <mailmeharshita24@gmail.com>
2022-11-26 13:03:49 +0530

    datasize

---
+++ JavaPractice/src/JavaBasics/DataTypeSizes.java
@@ -0,0 +1,23 @@
+package JavaBasics;
+
+public class DataTypeSizes {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Integer min value: "+ Integer.MIN_VALUE);
+		System.out.println("Integer max value: "+ Integer.MAX_VALUE);
+		System.out.println("Integer  byte: "+ Integer.BYTES);
+		
+		System.out.println("Byte min val: "+Byte.MIN_VALUE);
+		System.out.println("Byte max val: "+Byte.MAX_VALUE);
+		
+		System.out.println("Float min val: "+Float.MIN_VALUE);
+		System.out.println("Float max val: "+Float.MAX_VALUE);
+		
+		System.out.println("Short min value: "+ Short.MIN_VALUE);
+		System.out.println("Short max value: "+ Short.MAX_VALUE);
+		
+
+	}
+
+}
