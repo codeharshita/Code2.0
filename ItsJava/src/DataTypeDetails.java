537a5685bc2c1fc8ad999a7cbe46afe0a1971314
Harshita <mailmeharshita24@gmail.com>
2022-11-29 23:33:06 +0530

    DataTypes

---
+++ ItsJava/src/DataTypeDetails.java
@@ -0,0 +1,14 @@
+
+public class DataTypeDetails {
+
+	public static void main(String[] args) {
+		byte x = 5;
+		System.out.println(Integer.toBinaryString(x));
+		
+		byte y = -6;
+		System.out.println(Integer.toBinaryString(y));
+		
+
+	}
+
+}
