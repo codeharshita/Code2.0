526dc63a105228eb71168ab313712bee3cd97dbd
Harshita <mailmeharshita24@gmail.com>
2022-11-30 23:39:41 +0530

    initial commit

--- ItsJava/src/DataTypeDetails.java
+++ ItsJava/src/DataTypeDetails.java
@@ -1,4 +1,9 @@
 
+
+ /*
+  * All the piece of code we are writing here is all about the memory.
+  * that is all the variables, methods, objects, classes etc are all about memory.
+  * **/
 public class DataTypeDetails {
 
 	public static void main(String[] args) {
@@ -7,7 +12,8 @@ public class DataTypeDetails {
 		
 		byte y = -6;
 		System.out.println(Integer.toBinaryString(y));
-		
+		//here integer is a wrapper class which consist of a method to convert bytr data into binary 
+		// string.
 
 	}
 
