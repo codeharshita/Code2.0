1074345e12aac4ae67f518b21d1497cbb3cab759
Harshita <mailmeharshita24@gmail.com>
2022-12-28 19:56:47 +0530

    try catch practise

--- Oops/src/TryCatchCheck.java
+++ Oops/src/TryCatchCheck.java
@@ -1,7 +1,21 @@
 
 public class TryCatchCheck {
+	
+	public static void display() {
+		System.out.println("Display on the screen.");
+	}
+	
+	//Always static block executes then any other member inside a class.
+	static {
+		int x = 1;
+		int v = 3;
+		int f =  x+v;
+		System.out.println(f);
+	}
 
 	public static void main(String[] args) {
+		
+		TryCatchCheck.display();
 		
 		
 		try {
@@ -16,11 +30,17 @@ public class TryCatchCheck {
 			System.out.println(m);
 			System.out.println(c);
 			
+			//multiple catch block can be used only with one single try block. 
 		}catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("Enter valid array element");
 		}catch(ArithmeticException e) {
 			System.out.println("Enter valid number."+ e.getMessage()+" "+ e);
+			
+			//Finally block always executes in the end.
+		}finally {
+			System.out.println("Program is executed.");
 		}
+		
 
 	}
 
