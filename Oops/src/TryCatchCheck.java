2cb5a0c50dc8f2014f2accd7ca6585d017c35438
Harshita <mailmeharshita24@gmail.com>
2022-12-28 19:47:31 +0530

    Multiple catch block

--- Oops/src/TryCatchCheck.java
+++ Oops/src/TryCatchCheck.java
@@ -7,13 +7,19 @@ public class TryCatchCheck {
 		try {
 			int arr[] = {1,3,5,7};
 			int a =arr[0];
+			int d = 0;
+			int m = a/d; // will throw exception here.
 			int b = arr[10]; //will throw exception
+			
 			int c= b/a;
 			
+			System.out.println(m);
 			System.out.println(c);
 			
-		}catch(Exception e) {
-			System.out.println("Enter valid array element"+" "+ e);
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Enter valid array element");
+		}catch(ArithmeticException e) {
+			System.out.println("Enter valid number."+ e.getMessage()+" "+ e);
 		}
 
 	}
