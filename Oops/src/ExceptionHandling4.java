f6b22e421ca8a4b7734d172de0e48c47e6c3b4d5
Harshita <mailmeharshita24@gmail.com>
2023-01-01 21:00:31 +0530

    Exception Handling in java

---
+++ Oops/src/ExceptionHandling4.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class ExceptionHandling4 {
+
+	public static void main(String[] args)throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number.");
+		int n = sc.nextInt();
+		System.out.println("Enter the second number.");
+		int m = sc.nextInt();
+		
+		if(m == 3 ) {
+			throw new ArithmeticException("Error in the input.");
+		}
+		
+		try {
+		System.out.println(n/m);
+		System.out.println("Good going.");
+		sc.close();
+		}catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}catch(InputMismatchException e) {
+			System.out.println(e.getMessage());
+		}
+			System.out.println("Keep going.");
+	
+		
+
+	}
+
+}
