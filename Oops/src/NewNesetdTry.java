e277c2f3860dc0d0ddc62150dcd94ec668112320
Harshita <mailmeharshita24@gmail.com>
2023-01-02 10:37:59 +0530

    Nested try-catch block

---
+++ Oops/src/NewNesetdTry.java
@@ -0,0 +1,31 @@
+
+public class NewNesetdTry {
+
+	public static void main(String[] args) {
+	try {
+		System.out.println(3/0);
+		
+		
+		try {
+			System.out.println(4/0);
+		}
+		catch(ArithmeticException e) 
+		{
+			System.out.println("Inner try-catch block executed."+"Arithmetic Exception is caught.");
+		}
+		
+		
+		
+	}
+	catch(ArithmeticException e) {
+		System.out.println("Outer try-catch block executed."+"Arithmetic Exception is handled.");
+	}
+	
+	
+	
+	
+	
+	}
+	
+
+}
