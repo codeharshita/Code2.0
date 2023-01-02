d8d0cb9623a1c9f22ed9675a20b0a0dde2873934
Harshita <mailmeharshita24@gmail.com>
2023-01-02 10:01:10 +0530

    Constructor Exception

---
+++ Oops/src/ConstructorException.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Kid{
+	String name;
+	int age;
+	//Constructor can also throw exception.
+	public Kid() throws InputMismatchException {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter your name");
+		this.name = sc.next();
+		System.out.println("Enter your age.");
+		this.age = sc.nextInt();
+		sc.close();
+		
+	}
+}
+public class ConstructorException {
+
+	public static void main(String[] args) {
+		try {
+		Kid kid1 = new Kid();
+		}
+		catch(InputMismatchException ex) {
+			System.out.println("Enter valid input.");
+		}
+		
+		
+		
+
+	}
+
+}
