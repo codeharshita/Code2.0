81037f0e8f5f634a33e875bac4ca958967a59ec8
Harshita <mailmeharshita24@gmail.com>
2023-01-05 23:37:58 +0530

    Wrapper classes in java

---
+++ Oops/src/Wrapper.java
@@ -0,0 +1,31 @@
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		//int i = 4;
+		//Integer t = new Integer(10); 
+		
+		Integer w = Integer.valueOf(3);
+		System.out.println(w);
+		Integer m = Integer.valueOf("23");
+		System.out.println(m);
+		
+		
+		System.out.println("**************************");
+		byte bb =41;
+		Byte b1 = Byte.valueOf(bb);//boxing -> converting bb type to object.
+		System.out.println(b1);
+		
+		short s1 = 45;
+		Short s2 =  Short.valueOf(s1); // boxing -> converting s1 type to s2 object. 
+		System.out.println(s2);
+		Short m1 = Short.valueOf("432");
+		System.out.println(m1);
+		
+		
+		System.out.println("***************************");
+		Float f =Float.valueOf(3.4f);
+		System.out.println(f);
+	}
+
+}
