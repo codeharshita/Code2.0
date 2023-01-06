da9e05e5d553ee1a35f02311f5f33cbcc9a9bfcc
Harshita <mailmeharshita24@gmail.com>
2023-01-06 09:14:44 +0530

    Wrapper class demo

---
+++ Oops/src/WrapperDemo2.java
@@ -0,0 +1,29 @@
+/**
+ * Float, Character,Boolean Classes. 
+ * */
+public class WrapperDemo2 {
+
+	public static void main(String[] args) {
+		float f= 12.4f;
+		Float f2 = 12.4f;
+		Float f3 = (float)Math.sqrt(9);
+		
+		System.out.println(f2 == Float.NaN);
+		System.out.println(f3.isNaN());
+		
+		System.out.println("******************************");
+
+		Float m1 = Float.valueOf(f);
+		Float m2= Float.valueOf(f2);
+		
+		boolean k = m1.equals(m2);
+		System.out.println(k);
+		
+		System.out.println(m1.equals(m2));
+		
+		
+		
+		
+				}
+
+}
