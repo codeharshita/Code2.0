6d93b705ffd97a124c0f78449a694f257b4fe502
Harshita <mailmeharshita24@gmail.com>
2023-01-08 18:39:23 +0530

    Annotations

---
+++ Oops/src/AnnoDemo.java
@@ -0,0 +1,18 @@
+/**
+ * Creating a user-defined Annotation.
+ * 
+ * */
+import java.lang.annotation.Annotation;
+
+@interface MyDemo{
+	
+}
+
+public class AnnoDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
