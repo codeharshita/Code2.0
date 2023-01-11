4cdd527f62cc672830ad27d1f044b1fa4ca2167f
Harshita <mailmeharshita24@gmail.com>
2023-01-11 06:10:58 +0530

    Lambda Expression (Functional Interface)

---
+++ Oops/src/LambdaExp2.java
@@ -0,0 +1,41 @@
+@FunctionalInterface
+interface DemoAno{
+	public void meth1();
+}
+
+interface printName{
+	public String print(String name);
+}
+
+//class Abc implements DemoAno{
+//	public void meth1() {
+//		System.out.println("This is method 1.");
+//	}
+//}
+
+
+public class LambdaExp2 {
+
+	public static void main(String[] args) {
+	
+//		Abc a = new Abc();
+//		a.meth1();
+//		
+//		System.out.println();
+//		DemoAno dm = new Abc();
+//		dm.meth1();
+		
+		DemoAno dm =()->{
+			System.out.println("Hello method");
+		};
+		
+		dm.meth1();
+		//Lambda expressons works only with Functional Interface.
+		printName p =(str)->{
+			return str;
+			
+		};
+		System.out.println(p.print("Alexa"));
+	}
+
+}
