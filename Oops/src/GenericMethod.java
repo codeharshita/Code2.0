544d4ea014a2e1ec9c15fb7b0ebc7c8c6d59fc70
Harshita <mailmeharshita24@gmail.com>
2023-01-13 10:33:28 +0530

    Generics in java

---
+++ Oops/src/GenericMethod.java
@@ -0,0 +1,18 @@
+class MyGenericMethod {
+	
+	public <T> void genericsMethod(T data) {
+		System.out.println("Java programming");
+		System.out.println("Hello world." + data);
+	}
+	
+}
+
+public class GenericMethod {
+
+	public static void main(String[] args) {
+		 MyGenericMethod demo = new  MyGenericMethod();
+		 	demo.<String>genericsMethod("Generics Method.");
+		 	demo.<Integer>genericsMethod(25);
+		}
+
+}
