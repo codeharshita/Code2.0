7c92e35e6db9c5d8f99bf3eab7042fac427a2d66
Harshita <mailmeharshita24@gmail.com>
2023-01-15 10:00:32 +0530

    initial commit

--- Oops/src/MyGeneric2.java
+++ Oops/src/MyGeneric2.java
@@ -1,48 +1,45 @@
 // understanding generics.
 
 //DATA HIDING
-class DataNew<T>{
-	//Making the variable private.
+class DataNew<T> {
+	// Making the variable private.
 	private T obj;
-	
-	
+
 	// Property methods -> Using getter and setter method.
-	//setting the  data using logic.
+	// setting the data using logic.
 	public void setData(T v) {
 		obj = v;
-		
+
 	}
-	
-	//getting or retrieving the data using return.
-	 public T getData() {
+
+	// getting or retrieving the data using return.
+	public T getData() {
 		return obj;
 	}
-	
-		
+
 }
 
 public class MyGeneric2 {
-	
 
 	public static void main(String[] args) {
 		// Generic type Integer only.
-		DataNew<Integer> data =new DataNew<>();
+		DataNew<Integer> data = new DataNew<>();
 		data.setData(43);
-		
+
 		System.out.println(data.getData());
-		
+
 		// Generic Type String only.
-		DataNew <String> dataNew= new DataNew<>();
+		DataNew<String> dataNew = new DataNew<>();
 		dataNew.setData("Blair Bunoto");
-		
+
 		System.out.println(dataNew.getData());
 
 		// Generic Type Float only.
-		DataNew <Float> dataFloat= new DataNew<>();
+		DataNew<Float> dataFloat = new DataNew<>();
 		dataFloat.setData(3.4f);
-		
+
 		System.out.println(dataFloat.getData());
-		
+
 	}
 
 }
--- Oops/src/Wrapper.java
+++ Oops/src/Wrapper.java
@@ -1,53 +1,48 @@
 /**
- * Wrapper is also known as Wrapping or boxing 
- * It is used to convert Primitive types to objects through classes or vice-versa.
+ * Wrapper is also known as Wrapping or boxing It is used to convert Primitive
+ * types to objects through classes or vice-versa.
  * 
  * It is divided into two types :- AutoBoxing and Unboxing.
- * */
+ */
 public class Wrapper {
 
 	public static void main(String[] args) {
-		//int i = 4;
-		//Integer t = new Integer(10); 
-		
+		// int i = 4;
+		// Integer t = new Integer(10);
+
 		Integer w = Integer.valueOf(3);
 		System.out.println(w);
 		Integer m = Integer.valueOf("23");
 		System.out.println(m);
-		
-		
+
 		System.out.println("**************************");
-		byte bb =41;
-		Byte b1 = Byte.valueOf(bb);//boxing -> converting bb type to object.
+		byte bb = 41;
+		Byte b1 = Byte.valueOf(bb);// boxing -> converting bb type to object.
 		System.out.println(b1);
-		
+
 		short s1 = 45;
-		Short s2 =  Short.valueOf(s1); // boxing -> converting s1 type to s2 object. 
+		Short s2 = Short.valueOf(s1); // boxing -> converting s1 type to s2 object.
 		System.out.println(s2);
 		Short m1 = Short.valueOf("432");
 		System.out.println(m1);
-		
-		
+
 		System.out.println("***************************");
-		Float f =Float.valueOf(3.4f);
+		Float f = Float.valueOf(3.4f);
 		System.out.println(f);
-		
-		//Autoboxing :- Converting primitive data type into Wapper class object.
+
+		// Autoboxing :- Converting primitive data type into Wapper class object.
 		System.out.println("AUTOBOXING");
 		int k = 2;
 		Integer h = k;
 		System.out.println(h);
-		
-		//Unboxing:- Converting Wrapper class object(data) into primitive data type.
-		
+
+		// Unboxing:- Converting Wrapper class object(data) into primitive data type.
+
 		System.out.println("UNBOXING.");
-		Integer  var1 = new Integer(19);
+		Integer var1 = new Integer(19);
 		int var2 = var1;
 		System.out.println(var2);
-		
-		
-		
-		
+
 	}
 
 }
