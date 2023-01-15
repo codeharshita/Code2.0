486fd5d411626738eeca6be177a48fe1ff821e69
Harshita <mailmeharshita24@gmail.com>
2023-01-15 10:00:01 +0530

    Generics

--- Oops/src/GenericMethod.java
+++ Oops/src/GenericMethod.java
@@ -1,18 +1,19 @@
 class MyGenericMethod {
-	
+
 	public <T> void genericsMethod(T data) {
 		System.out.println("Java programming");
 		System.out.println("Hello world." + data);
+
 	}
-	
+
 }
 
 public class GenericMethod {
 
 	public static void main(String[] args) {
-		 MyGenericMethod demo = new  MyGenericMethod();
-		 	demo.<String>genericsMethod("Generics Method.");
-		 	demo.<Integer>genericsMethod(25);
-		}
+		MyGenericMethod demo = new MyGenericMethod();
+		demo.<String>genericsMethod("Generics Method.");
+		demo.<Integer>genericsMethod(25);
+	}
 
 }
--- Oops/src/MyGeneric3.java
+++ Oops/src/MyGeneric3.java
@@ -1,34 +1,30 @@
-class MyArray<T extends Number>{
-	
-	
+class MyArray<T extends Number> {
+
 	// Initilaize an array of generic type
-	
+
 	@SuppressWarnings("unchecked")
-	T A[]  = (T[]) new Object[10];
+	T A[] = (T[]) new Object[10];
 	int length = 0;
-	
+
 	public void append(T v) {
 		A[length++] = v;
 	}
-	
+
 	public void display() {
-		for(int i = 0; i<length; i++) {
+		for (int i = 0; i < length; i++) {
 			System.out.println(A[i]);
 		}
-	} 
-	
+	}
+
 }
 
-
-
-
 public class MyGeneric3 {
-	
-	public static <E>void print(E[] list) {
-		for(E x: list) {
+
+	public static <E> void print(E[] list) {
+		for (E x : list) {
 			System.out.println(x);
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -37,13 +33,10 @@ public class MyGeneric3 {
 		myArray.append(29.4f);
 		myArray.append(76.2f);
 		myArray.append(112.1f);
-		
+
 		myArray.display();
-		
-		print(new String[] {"HII" ,"JAVA", "Language." } ) ;
-		
-		
-		 
+
+		print(new String[] { "HII", "JAVA", "Language." });
 
 	}
 
