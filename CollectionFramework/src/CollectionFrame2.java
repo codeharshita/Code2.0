33886c707ae2dd7609f2d6f0e972eb2f9515e832
Harshita <mailmeharshita24@gmail.com>
2023-01-16 12:02:22 +0530

    ArrayList operations

---
+++ CollectionFramework/src/CollectionFrame2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class CollectionFrame2 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> al1 = new ArrayList<>();
+		ArrayList<Integer> al2 = new ArrayList<>(List.of(12, 32, 34, 76, 98, 50));
+		al1.add(43);
+		al1.add(21);
+		al1.add(30);
+		al1.add(80);
+		al1.add(84);
+		al1.add(67);
+		al1.add(0, 999);
+		al1.add(37);
+
+		System.out.println(al1.contains(80));
+		al1.remove(0);
+		System.out.println(al1.size() + " size of arraylist1");
+
+		al2.add(0, 31);
+		System.out.println("LIST" + al2 + al2.size());
+
+		System.out.println(al1);
+
+	}
+
+}
