01d70b574d4815ee29c6188878e79f97e5c89d06
Harshita <mailmeharshita24@gmail.com>
2023-01-18 07:36:10 +0530

    LambdaExpression

--- Oops/src/LambdaExp.java
+++ Oops/src/LambdaExp.java
@@ -1,61 +1,60 @@
 /**
- * Functional Interface are those interface which consists of only one abstract method.
+ * Functional Interface are those interface which consists of only one abstract
+ * method.
  * 
- * */
+ */
 
 @FunctionalInterface
 
-interface MyLambda{
+interface MyLambda {
 	public void display();
 }
 
-interface MyNewLambda{
-	public int sum(int a , int b);
+interface MyNewLambda {
+	public int sum(int a, int b);
 }
 
-interface PrintThis{
+interface PrintThis {
 	public void print(String s);
 }
 
-interface SumIt{
-	public int add(int c ,int d);
+interface SumIt {
+	public int add(int c, int d);
 }
 
-/*class World implements MyLambda{
-	public void display() {
-		System.out.println("This is my world.");
-	}
-	
-}*/
+/*
+ * class World implements MyLambda{ public void display() {
+ * System.out.println("This is my world."); }
+ * 
+ * }
+ */
 
 public class LambdaExp {
 
 	public static void main(String[] args) {
-		//Lambda Expression.
-		MyLambda m = ()-> {
-				System.out.println("This is my new World."); 
-			      };
-			
-		
+		// Lambda Expression.
+		MyLambda m = () -> {
+			System.out.println("This is my new World.");
+		};
+
 		m.display();
-		
-		MyNewLambda m1 = (int a, int b)->{
-			int c = a+b;
+
+		MyNewLambda m1 = (int a, int b) -> {
+			int c = a + b;
 			return c;
 		};
-		//m1.sum(3,2);
+		// m1.sum(3,2);
 		System.out.println(m1.sum(3, 2));
-		
-		PrintThis ptr =(s)->{
+
+		PrintThis ptr = (s) -> {
 			System.out.println(s);
 		};
-		
+
 		System.out.println("Hello, I am Lambda Expression.");
-		
-		SumIt sn = (a,b)->a+b;
-		int r = sn.add(30,20);
+
+		SumIt sn = (a, b) -> a + b;
+		int r = sn.add(30, 20);
 		System.out.println(r);
-		
 
 	}
 
