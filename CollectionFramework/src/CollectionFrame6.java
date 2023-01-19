cefd704877666b95f6a1f6bb5a2048ee8b4d570c
Harshita <mailmeharshita24@gmail.com>
2023-01-20 00:17:53 +0530

    doubt line 35 - Doubly linkedlist

--- CollectionFramework/src/CollectionFrame6.java
+++ CollectionFramework/src/CollectionFrame6.java
@@ -14,6 +14,8 @@ public class CollectionFrame6 {
 		ll2.push(54);
 		ll2.push(43);
 
+		System.out.println("Popped element: " + ll2.pop());
+
 		System.out.println(ll2.peek()); // show the first element , but will not remove
 		System.out.println(ll2.peekFirst()); // also shows the first element, but not remove.
 		System.out.println(ll2.peekLast()); // shows the last element or data or object, but not remove.
@@ -23,14 +25,14 @@ public class CollectionFrame6 {
 		}
 
 		System.out.println(" \nPolling: ");
-		System.out.println(ll2.poll());//remove the first element.
-		System.out.println(ll2.pollFirst());//also removes the first element.
+		System.out.println(ll2.poll());// remove the first element.
+		System.out.println(ll2.pollFirst());// also removes the first element.
 		System.out.println(ll2.pollLast());// also removes the last element.
 
 //		System.out.println(ll2.push(42));
 //		System.out.println(ll2.addFirst(54));
-		//ll2.push(45);
-		System.out.println(ll2.toArray()); //[Ljava.lang.Object;@3fee733d    ??
+		// ll2.push(45);
+		System.out.println(ll2.toArray()); // [Ljava.lang.Object;@3fee733d ??
 		System.out.println(ll2.toString());
 	}
 
