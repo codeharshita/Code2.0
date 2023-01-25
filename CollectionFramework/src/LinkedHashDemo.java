53626796869fbe6cb26f5384f224f38b84516a53
Harshita <mailmeharshita24@gmail.com>
2023-01-25 08:53:23 +0530

    LinkedHashMap implementation

--- CollectionFramework/src/LinkedHashDemo.java
+++ CollectionFramework/src/LinkedHashDemo.java
@@ -3,13 +3,27 @@ public class LinkedHashDemo {
 
 	public static void main(String[] args) {
 		
-		LinkedHashMap<Integer, String> lhm = new LinkedHashMap<>(6);
+		//(5)-> for insertion
+		// (5, .75f, true) -> for accessing.
+		LinkedHashMap<Integer, String> lhm = new LinkedHashMap<>(6, .75f,true) {
+			
+			protected boolean removeEldestEntry(Map.Entry e) {
+				return size()>5;
+			}
+		}; 
+		
 		lhm.put(1, "A");
 		lhm.put(2, "B");
 		lhm.put(3, "C");
 		lhm.put(4, "D");
 		lhm.put(5, "E");
 		lhm.put(6, "F");
+		
+		
+		String s= lhm.get(2);
+		s = lhm.get(5);
+		s= lhm.get(1);
+		
 		
 		lhm.forEach((k,v)->System.out.println(k+" "+" "+v)); 
 
