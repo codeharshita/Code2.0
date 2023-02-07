07c1f14bbe524f80a04a12e6fb549aaf5c4cf8e6
Harshita <mailmeharshita24@gmail.com>
2023-02-07 14:16:30 +0530

    Thread run, interrupt, sleep.

---
+++ MultiThreading Concept/src/ThreadTest7.java
@@ -0,0 +1,31 @@
+class MyClass2 extends Thread {
+
+	public MyClass2(String name) {
+		super(name);
+	}
+
+	public void run() {
+		int count = 1;
+		while (true) {
+			System.out.println(count++);
+
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+}
+
+public class ThreadTest7 {
+
+	public static void main(String[] args) {
+		MyClass2 obj = new MyClass2(" I AM THREAD");
+		obj.start();
+		obj.interrupt();
+
+	}
+
+}
